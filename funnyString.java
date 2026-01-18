56a3e7aa72e93505bb9278cd23a527a9385d61ac
souptik80070 <Souptik.Khatua2024@iem.edu.in>
2026-01-18 22:20:12 +0530

    Implement funnyString function in funnyString.java

---
+++ funnyString.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+      StringBuilder rev = new StringBuilder(s);
+  rev.reverse();
+  for(int i =0;i<s.length()-1;i++){
+    int a = Math.abs(((int)(s.charAt(i)))-(int)s.charAt(i+1));
+    int b = Math.abs(((int)(rev.charAt(i)))-(int)rev.charAt(i+1));
+    if(a!=b){
+        return "Not Funny";
+        
+    }
+  }
+  return "Funny";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
